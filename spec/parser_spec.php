378d4a5422359d2d2ee8f1dfcb42ab926a9b3af6
Harry Metcalfe <harry@thedextrousweb.com>
2009-12-12 17:52:25 +0000

    Run the positive tests

--- spec/parser_spec.php
+++ spec/parser_spec.php
@@ -8,7 +8,7 @@ class DescribeParser extends PHPSpec_Context
 
     try
     {
-      $fammel->parse(file_get_contents($file));
+      $fammel->parse_file($file);
     }
     catch(Exception $e)
     {
@@ -19,6 +19,48 @@ class DescribeParser extends PHPSpec_Context
 
     $this->fail();
     return false;
+  }
+  
+  function it_should_succeed_on_all_positive_tests()
+  {
+    $dir = dirname(__FILE__) .'/data/positive/';
+    
+    $dh = opendir($dir);
+    
+    while($file = readdir($dh))
+    {
+      if($file[0] == '.' || !preg_match('/\.haml$/', $file))
+      {
+        continue;
+      }
+      
+      $haml = $dir . $file;
+      $html = $dir . str_replace(".haml", ".html", $file);
+      
+      $fammel = new Fammel();
+      
+      try
+      {
+        $fammel->parse_file($haml);
+      }
+      catch(Exception $e)
+      {
+        // None of these should fail
+        $this->fail("Parsing failed: " . $e->getMessage());
+      }
+      
+      try
+      {
+        $rendered = $fammel->render();
+      }
+      catch(Exception $e)
+      {
+        // None of these should fail
+        $this->fail();
+      }  
+      
+      $this->spec($rendered)->should->equal(file_get_contents($html));
+    }
   }
 
   function it_should_fail_on_no_closing_brace()
